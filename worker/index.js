fc4822f142df3c9ddb38a443203b72111ea466d7
Robi Hidayat <robihdy@gmail.com>
2020-03-30 10:48:59 +0700

    change cron schedule

--- worker/index.js
+++ worker/index.js
@@ -2,7 +2,7 @@ const CronJob = require('cron').CronJob;
 const fetchGithub = require('./tasks/fetch-github');
 
 const job = new CronJob(
-  '0 1 * * *',
+  '* * * * *',
   fetchGithub,
   null,
   true,
